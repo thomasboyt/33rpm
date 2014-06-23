c8415275ce4de36f94743503aa803f1574045a22
Thomas Boyt <thomas.boyt@venmo.com>
2014-06-22 20:27:15 -0400

    blegh

--- app/app.js
+++ app/app.js
@@ -12,30 +12,86 @@
   var THIRTY_THREE_DEG = 0.198;  // http://www.wolframalpha.com/input/?i=33rpm+in+degrees%2Fms
   var FORTY_FIVE = 0.004712;  // http://www.wolframalpha.com/input/?i=45rpm+in+radians%2Fms
 
+
   var Game = function() {
-    this.c = new Coquette(this, 'canvas', 600, 600, 'silver');
+    this.c = new Coquette(this, 'canvas', 700, 600, 'silver');
 
-    var record = this.c.entities.create(Record, {});
-    var label = this.c.entities.create(Label, {
-      record: record
+    this.fsm = StateMachine.create({
+      initial: 'attract',
+      events: [
+        { name: 'start', from: ['attract', 'dead'], to: 'playing' },
+        { name: 'died', from: 'playing', to: 'dead' },
+        { name: 'pause', from: 'playing', to: 'paused' },
+        { name: 'unpause', from: 'paused', to: 'playing' }
+      ],
+      callbacks: {
+        onenterplaying: this.start.bind(this),
+        ondied: this.died.bind(this),
+        onenterstate: function(e, f, t) {
+          console.log('event:', e, '; transition:', f, '=>', t);
+        }
+      }
     });
 
-    var player = this.c.entities.create(Player, {
-      record: record
-    });
-    var barrier = this.c.entities.create(Barrier, {
-      record: record
+    this.record = this.c.entities.create(Record, {});
+    this.label = this.c.entities.create(Label, {
+      record: this.record,
+      paused: true
     });
 
-    setInterval(function() {
-      this.c.entities.create(Enemy, {
-        record: record,
-        lane: Math.floor(Math.random() * 4),
-        angle: barrier.angleFromCenter * (Math.PI/180)
-      });
-    }.bind(this), 1000);
+  };
+
+  Game.prototype.update = function(dt) {
+    if (this.fsm.is("attract")) {
+      if (this.c.inputter.isPressed(this.c.inputter.SPACE)) {
+        setTimeout(this.fsm.start.bind(this.fsm), 0);
+      }
+    } else if (this.fsm.is("dead")) {
+      if (this.c.inputter.isPressed(this.c.inputter.R)) {
+        setTimeout(this.fsm.start.bind(this.fsm), 0);
+      }
+    }
+  };
+
+  Game.prototype.start = function() {
+    if (this.barrier) {
+      this.c.entities.destroy(this.barrier);
+    }
 
     this.score = 0;
+    this.label.paused = false;
+    this.label.angle = 0;
+
+    var player = this.c.entities.create(Player, {
+      record: this.record
+    });
+    this.barrier = this.c.entities.create(Barrier, {
+      record: this.record
+    });
+
+    this.spawner = setInterval(function() {
+      this.c.entities.create(Enemy, {
+        record: this.record,
+        lane: Math.floor(Math.random() * 4),
+        angle: this.barrier.angleFromCenter * (Math.PI/180)
+      });
+    }.bind(this), 1000);
+  };
+
+  Game.prototype.destroyAll = function() {
+    [Player, Enemy, Bullet].forEach(function(constructor) {
+      var items = this.c.entities.all(constructor);
+      items.forEach(function(item) {
+        this.c.entities.destroy(item);
+      }.bind(this));
+    }.bind(this));
+  };
+
+  Game.prototype.died = function() {
+    // pause action so you can see that you messed up
+    this.destroyAll();
+    clearInterval(this.spawner);
+    this.label.paused = true;
   };
 
 
@@ -196,9 +252,9 @@
     ctx.closePath();
   };
 
-  Enemy.prototype.collision = function(other) {
+  Enemy.prototype.collision = function(other, type) {
     if ( other instanceof Player ) {
-      console.log('DEADED');
+      this.game.fsm.died();
     } else if ( other instanceof Barrier ) {
       if ( !this.spawnImmunity ) {
         this.game.c.entities.destroy(this);
@@ -275,6 +331,7 @@
 
   var Label = function(game, opts) {
     this.center = opts.record.center;
+    this.paused = opts.paused;
 
     this.imageObj = new Image();
     this.imageObj.src = 'data/33rpm_1x.png';
@@ -282,12 +339,14 @@
   };
 
   Label.prototype.update = function(step) {
-    var overflow = 360;
-    this.angle = (this.angle + (THIRTY_THREE_DEG * step)) % overflow;
+    if (!this.paused) {
+      var overflow = 360;
+      this.angle = (this.angle + (THIRTY_THREE_DEG * step)) % overflow;
+    }
   };
 
   Label.prototype.draw = function(ctx) {
-    ctx.drawImage(this.imageObj, this.center.x - this.imageObj.width / 2, this.center.y - this.imageObj.height / 2);
+    ctx.drawImage(this.imageObj, this.center.x - this.imageObj.width / 2 + 0.5, this.center.y - this.imageObj.height / 2 + 0.5);
   };
 
   global.Game = Game;
