86f5f356ed01c4ed47a35564950a0d0954492f44
Thomas Boyt <me@thomasboyt.com>
2014-06-28 20:41:04 -0400

    Remove unused constants

--- app/constants.js
+++
@@ -1,5 +0,0 @@
-export default {
-  THIRTY_THREE: 0.003456,  // http://www.wolframalpha.com/input/?i=33rpm+in+radians%2Fms
-  THIRTY_THREE_DEG: 0.198,  // http://www.wolframalpha.com/input/?i=33rpm+in+degrees%2Fms
-  FORTY_FIVE: 0.004712  // http://www.wolframalpha.com/input/?i=45rpm+in+radians%2Fms
-};
